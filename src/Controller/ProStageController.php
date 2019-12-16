23261a84fafe044a887f0c4adc7e295de6fbfca7
Mehdi Brini <mbrini@iutbayonne.univ-pau.fr>
2019-12-16 18:06:04 +0100

    Modification du route.yaml -> déplacement des routes du controleur vers le fichier en question

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -7,33 +7,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/", name="pro_stage_accueil")
-     */
+    
     public function index()
     {
         return $this->render('pro_stage/index.html.twig');
     }
 
-    /**
-     * @Route("/entreprises", name="pro_stage_entreprises")
-     */
+   
     public function afficherEntreprises()
     { 
         return $this->render('pro_stage/affichageEntreprise.html.twig');
     }
 
-    /**
-     * @Route("/formations", name="pro_stage_formations")
-     */
+   
     public function afficherFormations()
     { 
         return $this->render('pro_stage/affichageFormations.html.twig');
     }
 
-     /**
-     * @Route("/stages/{leID}", name="pro_stage_stages")
-     */
+
     public function afficherStages($leID)
     { 
         return $this->render('pro_stage/affichageStages.html.twig', ['idStage'=> $leID]);
