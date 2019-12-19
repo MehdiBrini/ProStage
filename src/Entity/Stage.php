34ba30b75bcf523d0791a6c8c7854d1bf1d435c7
Mehdi Brini <mbrini@iutbayonne.univ-pau.fr>
2019-12-19 22:19:53 +0100

    Création et migration de l'entitée Stage

---
+++ src/Entity/Stage.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StageRepository")
+ */
+class Stage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", length=8)
+     */
+    private $idStage;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $intitule;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $domaine;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $nomEntreprise;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $formation;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $lieu;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $contactMail;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdStage(): ?string
+    {
+        return $this->idStage;
+    }
+
+    public function setIdStage(string $idStage): self
+    {
+        $this->idStage = $idStage;
+
+        return $this;
+    }
+
+    public function getIntitule(): ?string
+    {
+        return $this->intitule;
+    }
+
+    public function setIntitule(string $intitule): self
+    {
+        $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    public function getDomaine(): ?string
+    {
+        return $this->domaine;
+    }
+
+    public function setDomaine(string $domaine): self
+    {
+        $this->domaine = $domaine;
+
+        return $this;
+    }
+
+    public function getNomEntreprise(): ?string
+    {
+        return $this->nomEntreprise;
+    }
+
+    public function setNomEntreprise(string $nomEntreprise): self
+    {
+        $this->nomEntreprise = $nomEntreprise;
+
+        return $this;
+    }
+
+    public function getFormation(): ?string
+    {
+        return $this->formation;
+    }
+
+    public function setFormation(string $formation): self
+    {
+        $this->formation = $formation;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getContactMail(): ?string
+    {
+        return $this->contactMail;
+    }
+
+    public function setContactMail(string $contactMail): self
+    {
+        $this->contactMail = $contactMail;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20191219211335.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191219211335 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE stage (id INT AUTO_INCREMENT NOT NULL, id_stage INT NOT NULL, intitule VARCHAR(200) NOT NULL, domaine VARCHAR(200) NOT NULL, nom_entreprise VARCHAR(200) NOT NULL, formation VARCHAR(200) NOT NULL, lieu VARCHAR(200) NOT NULL, contact_mail VARCHAR(200) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE stage');
+    }
+}
