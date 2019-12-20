4f91c9a4bdc468ab82241316b6863db51317c45f
Mehdi Brini <mbrini@iutbayonne.univ-pau.fr>
2019-12-20 02:45:43 +0100

    création d'un module de test d'entite Entreprise -> Pangolin Web

--- src/DataFixtures/TestEntreprise.php
+++ src/DataFixtures/TestEntreprise.php
@@ -4,13 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Entreprise;
 
 class TestEntreprise extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $PangolinWeb = new Entreprise();
+        $PangolinWeb->setNom("Pangolin Web");
+        $PangolinWeb->setSiegeSocial("Anglet");
+        $PangolinWeb->setSiteWeb("www.pangolin-web.fr");
+        $PangolinWeb->setNumTel("0559547369");
+        $PangolinWeb->setAdresseMail("contact@pangolin-web.fr");
+
+        $manager->persist($PangolinWeb);
 
         $manager->flush();
     }
--- src/DataFixtures/TestStage.php
+++ src/DataFixtures/TestStage.php
@@ -4,14 +4,27 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Stage;
 
 class TestStage extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+         $StageDevWeb = new Stage();
+         $StageDevWeb->setIdStage(1);
+         $StageDevWeb->setIntitule("Stage en developpement web sous Symfony");
+         $StageDevWeb->setDomaine("Developpement Web");
+         $StageDevWeb->setNomEntreprise("Pangolin Web");
+         $StageDevWeb->setFormation("DUT Informatique");
+         $StageDevWeb->setLieu("Anglet");
+         $StageDevWeb->setContactMail("contact@PangolinWeb.fr");
+         $
+
+
+         $manager->persist($StageDevWeb);
 
         $manager->flush();
+        
+        
     }
 }
